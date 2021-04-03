d8ad0d0ffa340d378f8a90e202b47314d285ec07
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-03 19:58:40 -0300

    added global styles.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,11 @@ import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 import history from './services/history';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <Router history={history}>
+    <GlobalStyle />
     <Routes />
   </Router>
 );
