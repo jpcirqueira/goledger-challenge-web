848eb64f850a9e17e8191b7cb9c15a79f74cf4bf
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-03 22:38:40 -0300

    added component addButton.

---
+++ web/src/components/addButton/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MdAdd } from 'react-icons/md';
+
+import history from '../../services/history';
+// eslint-disable-next-line object-curly-newline
+import { Button, TextButton, Container } from './styles';
+
+interface StoreAddButtonProps {
+  name: string;
+  route: string;
+}
+// eslint-disable-next-line react/prop-types
+const AddButton: React.FC<StoreAddButtonProps> = ({ name, route }) => (
+  <>
+    <Container>
+      <Button onClick={() => history.push(`/${route}`)}>
+        <MdAdd size={30} color="#ecf0f1" />
+        <TextButton>{name}</TextButton>
+      </Button>
+    </Container>
+  </>
+);
+
+export default AddButton;
---
+++ web/src/components/addButton/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background: #4267b2;
+  width: 220px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-color: #4267b2;
+  border-radius: 4px;
+  border: none;
+  margin-right: 2%;
+`;
+
+export const TextButton = styled.p`
+  font-size: 20px;
+  margin-left: 20px;
+  color: #ecf0f1;
+  font-weight: bold;
+`;
+
+export const Container = styled.div`
+  margin-top: 2%;
+
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
