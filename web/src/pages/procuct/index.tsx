9acf69b5929f5e081ebedc717c4c7df4f1eaac14
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-04 16:14:28 -0300

    list all product.

--- web/src/pages/procuct/index.tsx
+++ web/src/pages/procuct/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
 
 import AddButton from '../../components/addButton';
 import ProductCard from '../../components/productCard';
-
+import api from '../../services/api';
 import { Container } from './styles';
 
-const Product: React.FC = () => (
-  <>
-    <AddButton name="New Product" route="addproduct" />
-    <Container>
-      <ProductCard code="401" name="notebook" price="1020.12" />
-    </Container>
-  </>
-);
+const Product: React.FC = () => {
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const body = {
+      query: {
+        selector: {
+          '@assetType': 'product',
+        },
+      },
+    };
+    api.post('query/search', body).then((res: any) => {
+      setProducts(res.data.result);
+    });
+  }, []);
+
+  return (
+    <>
+      <AddButton name="New Product" route="addproduct" />
+      <Container>
+        {products.map((prod: any) => (
+          <ProductCard
+            key={prod.key}
+            code={prod.code}
+            name={prod.name}
+            price={prod.price}
+          />
+        ))}
+      </Container>
+    </>
+  );
+};
 export default Product;
